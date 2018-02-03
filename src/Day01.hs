9909bbbb0ba7090d923673a23dc260491d600c37
Roland Tritsch <roland@gonito.com>
2018-02-03 08:55:11 +0000

    Day01: Part1: Tests are working

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,5 +1,20 @@
 -- Day01.hs
 module Day01 where
 
+import Data.Char (digitToInt)
+
+-- |`captcha` returns the captcha for the given number.
+-- Algorithm goes like this ...
+-- * turn the int into a List of Chars
+-- * append the first element of the list to the end of the list
+-- * move a sliding window of two elements over the list
+-- * add up all windows where the elements are the same
 captcha :: Int -> Int
-captcha i = 0
+captcha i = go (prep i) where
+  prep i = do
+    let digits = map digitToInt (show i)
+    digits ++ [head digits]
+  go (i : []) = 0
+  go (i : is)
+    | i == (head is) = i + go is
+    | otherwise = go is

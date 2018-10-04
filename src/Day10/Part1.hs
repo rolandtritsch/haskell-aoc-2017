af750bac9397b0bc771942b41a2d4a8482488501
Roland Tritsch <roland@tritsch.org>
2018-10-04 09:38:13 +0100

    Day10: Part2: Done.

--- src/Day10/Part1.hs
+++ src/Day10/Part1.hs
@@ -4,5 +4,5 @@ module Day10.Part1 where
 import Day10
 
 -- | solve the puzzle
-solve :: [Length] -> Int
-solve lengths = foldl (*) 1 $ take 2 $ knot lengths
+solve :: String -> Int
+solve input' = foldl (*) 1 $ take 2 $ hash $ knot (input2Lengths input') seed
--- src/Day10/Part2.hs
+++ src/Day10/Part2.hs
@@ -4,5 +4,5 @@ module Day10.Part2 where
 import Day10
 
 -- | solve the puzzle
-solve :: [Length] -> String
-solve _ = "2f8c3d2100fdd57cec130d928b0fd2dd"
+solve :: String -> String
+solve input' = dense2hex $ sparse2dense $ hash $ knots (encode input') rounds

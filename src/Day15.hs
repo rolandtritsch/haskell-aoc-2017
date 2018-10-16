b746571293080781121669ba5bca4bf50c8e8347
Roland Tritsch <roland@tritsch.org>
2018-10-16 08:52:48 +0100

    Day15: Done.

--- src/Day15.hs
+++ src/Day15.hs
@@ -16,8 +16,14 @@ Part2 - solve the puzzle with a modolo of (4, 8) and a depth of 5 * 10e6.
 -}
 module Day15 where
 
---import Util
+import Data.Bits ((.&.))
 
--- | read the input
-input :: String
-input = ""
+-- | generate an infinte list of elements (based on a/the generator config)
+generator :: (Int, Int, Int, Int) -> [Int]
+generator (start, factor, divider, multiple) = tail $ filter (criteria multiple) $ iterate next start where
+  criteria multiple' value = (mod value multiple') == 0
+  next previous = rem (previous * factor) divider
+
+-- | check, if the two numbers match (lower 16 bits are equal)
+matching :: Int -> Int -> Bool
+matching a b = (a .&. 0xffff) == (b .&. 0xffff)

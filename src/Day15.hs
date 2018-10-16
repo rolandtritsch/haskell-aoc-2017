3d2acdab60cb576eab76975f66ee6435dfec70c2
Roland Tritsch <roland@tritsch.org>
2018-10-16 09:31:05 +0100

    Day16: Initial commit.

--- src/Day15.hs
+++ src/Day15.hs
@@ -3,12 +3,7 @@ Problem: <https://adventofcode.com/2017/day/15>
 
 Solution:
 
-General - This was heavily refactored. Twice :). I started with a stream
-based/recursive approach. Then I tried to *just* use the stream (with take/count),
-but stream is an IteraterableAgain and that means it keeps the front of the stream
-around and that means we (sooner or later) run out of mem (initially using Strings
-did not help either; with the mem and the performance). The right approach is
-obviously to use an Iterator. And then do a recursion.
+General - (Much) Easi(er) in Haskell. Just use iterate right.
 
 Part1 - solve the puzzle with a modolo of (1, 1) and a depth of 40 * 10e6.
 

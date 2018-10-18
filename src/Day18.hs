43323aad73f05e72288181d02f5cb8d4d62da84f
Roland Tritsch <roland@tritsch.org>
2018-10-18 05:59:35 +0100

    Day18: Part1: Checkpoint. Exception thrown.

--- src/Day18.hs
+++ src/Day18.hs
@@ -28,8 +28,78 @@ writeCount.
 -}
 module Day18 where
 
+import qualified Data.Map.Strict as M
+
 import Util
+
+type Register = Char
+type Value = Int
+type Registers = M.Map Register Value
+
+type Instruction = State -> State
+type Counter = Int
+
+-- | the program state (pc, regs, done, exit)
+type State = (Counter, Registers, Bool, Int)
 
 -- | read the input
 input :: [String]
 input = inputRaw "input/Day18input.txt"
+
+-- | a/the special register for a/the sound value
+soundRegister :: Char
+soundRegister = 'S'
+
+-- | a/the special register for a/the sound recovery value
+recoveryRegister :: Char
+recoveryRegister = 'R'
+
+-- | excute the snd (send) instruction
+snd' :: Value -> State -> State
+snd' frequency (pc, rs, _, _) =  (pc + 1, M.insert soundRegister frequency rs, False, 0)
+
+-- | execute the set instruction
+set' :: Register -> Value -> State -> State
+set' r value (pc, rs, _, _) = (pc + 1, M.insert r value rs, False, 0)
+
+-- | execute the add instruction
+add' :: Register -> Value -> State -> State
+add' r value (pc, rs, _, _) = (pc + 1, M.insert r ((M.findWithDefault 0 r rs) + value) rs, False, 0)
+
+-- | execute the mul instruction
+mul' :: Register -> Value -> State -> State
+mul' r value (pc, rs, _, _) = (pc + 1, M.insert r ((M.findWithDefault 0 r rs) * value) rs, False, 0)
+
+-- | execute the mod instruction
+mod' :: Register -> Value -> State -> State
+mod' r value (pc, rs, _, _) = (pc + 1, M.insert r (mod (M.findWithDefault 0 r rs) value) rs, False, 0)
+
+-- | execute the rcv (receive) instruction
+rcv' :: Register -> State -> State
+rcv' r (pc, rs, _, _)
+  | (M.findWithDefault 0 r rs) == 0 = (0, rs, True, rs M.! soundRegister)
+  | otherwise = (pc + 1, rs, False, 0)
+
+-- | execute the jgz (jump, if greater than zero) instruction
+jgz' :: Register -> Int -> State -> State
+jgz' r offset (pc, rs, _, _)
+  | (M.findWithDefault 0 r rs) > 0 = (pc + offset, rs, False, 0)
+  | otherwise = (pc + 1, rs, False, 0)
+
+-- | build a/the program (a/the list of instructions) (from the input)
+instructions :: [String] -> [Instruction]
+instructions input' = map (instruction . words) input' where
+  instruction ("snd":arguments) = snd' (read (arguments !! 0))
+  instruction ("set":arguments) = set' ((arguments !! 0) !! 0) (read (arguments !! 1))
+  instruction ("add":arguments) = add' ((arguments !! 0) !! 0) (read (arguments !! 1))
+  instruction ("mul":arguments) = mul' ((arguments !! 0) !! 0) (read (arguments !! 1))
+  instruction ("mod":arguments) = mod' ((arguments !! 0) !! 0) (read (arguments !! 1))
+  instruction ("rcv":arguments) = rcv' ((arguments !! 0) !! 0)
+  instruction ("jgz":arguments) = jgz' ((arguments !! 0) !! 0) (read (arguments !! 1))
+  instruction _ = error "Unknown instruction"
+
+ -- | run the instructions (until we are done)
+run :: State -> [Instruction] -> Int
+run (_, _, True, exit) _ = exit
+run currentState@(pc, _, _, _) program = run nextState program where
+  nextState = (program !! pc) currentState

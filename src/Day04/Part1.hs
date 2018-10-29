1c928be129a5d12aa73e7964ff18d4004e43fab4
Roland Tritsch <roland@tritsch.org>
2018-10-29 05:54:48 +0000

    Day04: Minior refactoring (to make it compile again).

--- src/Day04/Part1.hs
+++ src/Day04/Part1.hs
@@ -4,13 +4,7 @@ import Text.Printf (printf)
 import System.TimeIt (timeItT)
 import Control.Exception.Base (evaluate)
 
-import Data.List.Unique
-
 import Day04
-
--- | check, if a given passphrase is valid
-isValid :: [String] -> Bool
-isValid phrase = repeated phrase == []
 
 -- | solve the puzzle. Input is a list of passphrases (every passphrase
 -- is a list of words). Return the number of valid passphrases.
--- src/Day04/Part2.hs
+++ src/Day04/Part2.hs
@@ -7,7 +7,6 @@ import Control.Exception.Base (evaluate)
 import Data.List (sort)
 
 import Day04
-import qualified Day04.Part1 as P1
 
 -- | to find the anagrams, we just need to sort the chars in the words first.
 sortCharsInWords :: [[String]] -> [[String]]
@@ -16,7 +15,7 @@ sortCharsInWords = map $ map sort
 -- | solve the puzzle. Input is a list of passphrases (every passphrase
 -- is a list of words). Return the number of valid passphrases.
 solve :: [[String]] -> Int
-solve phrases = countValid P1.isValid $ sortCharsInWords $ phrases
+solve phrases = countValid isValid $ sortCharsInWords $ phrases
 
 -- | main
 main :: IO ()
